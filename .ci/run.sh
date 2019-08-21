3b6d091f00959b44228913f908c654d800d2ee15
Kevin <kcarella@ucsc.edu>
2019-08-21 13:54:21 -0700

    Added correct filepath to run flake8 and its config file.

--- .ci/run.sh
+++ .ci/run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+readonly THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 trap exit SIGINT
 
-python3 -m flake8 ../pacai --config=./flake8.cfg
+python3 -m flake8 "${THIS_DIR}/../pacai" --config="${THIS_DIR}/flake8.cfg"
 if [[ $? -eq 0 ]]; then
     echo "Style passed!"
 else
