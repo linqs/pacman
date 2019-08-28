9d9d544e7d9545659f6650467300d50ae08dde92
kcarella <kcarella@ucsc.edu>
2019-08-28 16:26:19 -0700

    Removed extra newlines from resolving merge conflict

--- .ci/run.sh
+++ .ci/run.sh
@@ -4,12 +4,10 @@ readonly THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 trap exit SIGINT
 
-
 exitStatus=0
 
 # Check style.
 echo "Checking style ..."
-
 
 python3 -m flake8 "${THIS_DIR}/../pacai" --config="${THIS_DIR}/flake8.cfg"
 if [[ $? -eq 0 ]]; then
