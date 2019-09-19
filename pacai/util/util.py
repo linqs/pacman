b9effdcbe0254407c66ddf91934fa7f50cb4c430
Eriq Augustine <eriq.augustine@gmail.com>
2019-09-18 22:32:12 -0700

    Removed timeout functions that are no longer used.

--- pacai/util/util.py
+++ pacai/util/util.py
@@ -1,5 +1,3 @@
-import signal
-
 """
 Functions that could be used for various course projects.
 """
@@ -50,32 +48,3 @@ def matrixAsList(matrix, value = True):
                 cells.append((row, col))
 
     return cells
-
-# Code to handle timeouts.
-class TimeoutFunctionException(Exception):
-    """
-    Exception to raise on a timeout
-    """
-
-    pass
-
-class TimeoutFunction:
-    def __init__(self, function, timeout):
-        self.timeout = timeout
-        self.function = function
-
-    def handle_timeout(self, signum, frame):
-        raise TimeoutFunctionException()
-
-    def __call__(self, *args):
-        if 'SIGALRM' not in dir(signal):
-            return self.function(*args)
-        old = signal.signal(signal.SIGALRM, self.handle_timeout)
-        signal.alarm(self.timeout)
-        try:
-            result = self.function(*args)
-        finally:
-            signal.signal(signal.SIGALRM, old)
-
-        signal.alarm(0)
-        return result
