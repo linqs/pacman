211b34a0c2641077375914a1f8bb81c79b6ab3de
Eriq Augustine <eriq.augustine@gmail.com>
2019-09-21 23:19:38 -0700

    Added an initial prototype for converting frames to images/gifs.

--- pacai/ui/view.py
+++ pacai/ui/view.py
@@ -1,7 +1,17 @@
 import abc
 
-DEFAULT_PAUSE_TIME = 0
+from PIL import Image
+from PIL import ImageDraw
+
+# TODO(eriq): This should eventually be false.
+DEFAULT_SAVE_FRAMES = True
 DEFAULT_SAVE_EVERY_N_FRAMES = 5
+
+SQUARE_SIZE = 30
+
+GIF_FPS = 10
+GIF_FRAME_DURATION_MS = int(1.0 / GIF_FPS * 1000.0)
+GIF_FILENAME = 'test.gif'
 
 # TODO(eriq): This is specific for the Pacman-style games.
 class AbstractView(abc.ABC):
@@ -10,8 +20,9 @@ class AbstractView(abc.ABC):
     view should implement.
     """
 
-    def __init__(self, saveEveryNFrames = DEFAULT_SAVE_EVERY_N_FRAMES):
-        self._saveFrames = False
+    def __init__(self,
+            saveFrames = DEFAULT_SAVE_FRAMES, saveEveryNFrames = DEFAULT_SAVE_EVERY_N_FRAMES):
+        self._saveFrames = saveFrames
         self._saveEveryNFrames = saveEveryNFrames
         self._frameCount = 0
         self._keyFrames = []
@@ -21,7 +32,10 @@ class AbstractView(abc.ABC):
         Signal that the game is over and the UI should cleanup.
         """
 
-        pass
+        if (self._saveFrames and len(self._keyFrames) > 0):
+            images = [frame.toImage() for frame in self._keyFrames]
+            images[0].save(GIF_FILENAME, save_all = True, append_images = images,
+                    duration = GIF_FRAME_DURATION_MS, loop = 0, optimize = True)
 
     def initialize(self, state):
         """
@@ -39,7 +53,8 @@ class AbstractView(abc.ABC):
             forceDraw = True
 
         frame = Frame(state)
-        if (self._saveFrames and self._frameCount % self._saveEveryNFrames == 0):
+        if (state.isOver()
+                or (self._saveFrames and self._frameCount % self._saveEveryNFrames == 0)):
             self._keyFrames.append(frame)
 
         self._drawFrame(state, frame, forceDraw = forceDraw)
@@ -52,14 +67,6 @@ class AbstractView(abc.ABC):
         From a frame, output to whatever medium this view utilizes.
         """
 
-        pass
-
-    def _frameToImage(self, frame):
-        """
-        Create an in-memory image from a frame.
-        """
-
-        # TODO(eriq)
         pass
 
     def pause(self):
@@ -122,11 +129,14 @@ class Frame(object):
             for y in range(self._height):
                 if (state.hasWall(x, y)):
                     items[y] = Frame.WALL
-                elif (state.hasFood(x, y)):
+
+                if (state.hasFood(x, y)):
                     items[y] = Frame.FOOD
-                elif (state.hasCapsule(x, y)):
+
+                if (state.hasCapsule(x, y)):
                     items[y] = Frame.CAPSULE
-                elif ((x, y) in agentTokens):
+
+                if ((x, y) in agentTokens):
                     items[y] = agentTokens[(x, y)]
 
             board[x] = items
@@ -152,3 +162,40 @@ class Frame(object):
                 tokens[position] = Frame.GHOST_1 + (agentIndex - 1)
 
         return tokens
+
+    def toImage(self):
+        image = Image.new('RGB', (self._width * SQUARE_SIZE, self._height * SQUARE_SIZE))
+
+        draw = ImageDraw.Draw(image)
+
+        for x in range(self._width):
+            for y in range(self._height):
+                color = self._tokenToColor(self._board[x][y])
+
+                startPoint = self._toImageCoords(x, y)
+                endPoint = self._toImageCoords(x + 1, y - 1)
+                draw.rectangle([startPoint, endPoint], fill = color)
+
+        return image
+
+    def _toImageCoords(self, x, y):
+        # PIL has (0, 0) as the upper-left, while pacai has it as the lower-left.
+        return (x * SQUARE_SIZE, (self._height - 1 - y) * SQUARE_SIZE)
+
+    def _tokenToColor(self, token):
+        if (token == Frame.EMPTY):
+            return (0, 0, 0)
+        if (token == Frame.WALL):
+            return (0, 51, 255)
+        if (token == Frame.FOOD):
+            return (255, 255, 255)
+        elif (token == Frame.CAPSULE):
+            return (255, 0, 255)
+        elif (token >= Frame.GHOST_1 and token <= Frame.GHOST_6):
+            return (229, 0, 0)
+        elif (token >= Frame.PACMAN_1 and token <= Frame.PACMAN_6):
+            return (255, 255, 61)
+        elif (token == Frame.SCARED_GHOST):
+            return (0, 255, 0)
+        else:
+            return (0, 0, 0)
