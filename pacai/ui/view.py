4f58857e3284ca8c418a65a1c4e00238cc16e605
Eriq Augustine <eriq.augustine@gmail.com>
2019-09-22 16:59:55 -0700

    Added directional sprites for agents.

--- pacai/ui/view.py
+++ pacai/ui/view.py
@@ -4,6 +4,7 @@ import os
 from PIL import Image
 from PIL import ImageDraw
 
+from pacai.core.directions import Directions
 from pacai.util import util
 
 # TODO(eriq): This should eventually be false.
@@ -124,46 +125,110 @@ class Frame(object):
     A general representation of that can be seen on-screen at a given time.
     """
 
+    NORTH = 1
+    EAST = 2
+    SOUTH = 3
+    WEST = 4
+
     # For the walls, we have a different sprite depending on what sides (lines) are present.
     # An 'X' in the name indicates that a wall is not there.
     # To get the pacman "tubular" look, adjacent walls will look connected
     # and not have a line between them.
+    # Walls start at 100.
     WALL_NESW = _computeWallCode(False, False, False, False)
     WALL_NESX = _computeWallCode(False, False, False, True)
-    WALL_NEXW = _computeWallCode(False, False, True,  False)
-    WALL_NEXX = _computeWallCode(False, False, True,  True)
-    WALL_NXSW = _computeWallCode(False, True,  False, False)
-    WALL_NXSX = _computeWallCode(False, True,  False, True)
-    WALL_NXXW = _computeWallCode(False, True,  True,  False)
-    WALL_NXXX = _computeWallCode(False, True,  True,  True)
-    WALL_XESW = _computeWallCode(True,  False, False, False)
-    WALL_XESX = _computeWallCode(True,  False, False, True)
-    WALL_XEXW = _computeWallCode(True,  False, True,  False)
-    WALL_XEXX = _computeWallCode(True,  False, True,  True)
-    WALL_XXSW = _computeWallCode(True,  True,  False, False)
-    WALL_XXSX = _computeWallCode(True,  True,  False, True)
-    WALL_XXXW = _computeWallCode(True,  True,  True,  False)
-    WALL_XXXX = _computeWallCode(True,  True,  True,  True)
-
+    WALL_NEXW = _computeWallCode(False, False, True, False)
+    WALL_NEXX = _computeWallCode(False, False, True, True)
+    WALL_NXSW = _computeWallCode(False, True, False, False)
+    WALL_NXSX = _computeWallCode(False, True, False, True)
+    WALL_NXXW = _computeWallCode(False, True, True, False)
+    WALL_NXXX = _computeWallCode(False, True, True, True)
+    WALL_XESW = _computeWallCode(True, False, False, False)
+    WALL_XESX = _computeWallCode(True, False, False, True)
+    WALL_XEXW = _computeWallCode(True, False, True, False)
+    WALL_XEXX = _computeWallCode(True, False, True, True)
+    WALL_XXSW = _computeWallCode(True, True, False, False)
+    WALL_XXSX = _computeWallCode(True, True, False, True)
+    WALL_XXXW = _computeWallCode(True, True, True, False)
+    WALL_XXXX = _computeWallCode(True, True, True, True)
 
     # Token to mark what can occupy different locations.
     EMPTY = 0
-    WALL = 1
-    FOOD = 2
-    CAPSULE = 3
-    PACMAN_1 = 10
-    PACMAN_2 = 11
-    PACMAN_3 = 12
-    PACMAN_4 = 13
-    PACMAN_5 = 14
-    PACMAN_6 = 15
-    GHOST_1 = 20
-    GHOST_2 = 21
-    GHOST_3 = 22
-    GHOST_4 = 23
-    GHOST_5 = 24
-    GHOST_6 = 25
-    SCARED_GHOST = 30
+    FOOD = 1
+    CAPSULE = 2
+    SCARED_GHOST = 3
+
+    PACMAN_1 = 210
+    PACMAN_1N = PACMAN_1 + NORTH
+    PACMAN_1E = PACMAN_1 + EAST
+    PACMAN_1S = PACMAN_1 + SOUTH
+    PACMAN_1W = PACMAN_1 + WEST
+
+    PACMAN_2 = 220
+    PACMAN_2N = PACMAN_2 + NORTH
+    PACMAN_2E = PACMAN_2 + EAST
+    PACMAN_2S = PACMAN_2 + SOUTH
+    PACMAN_2W = PACMAN_2 + WEST
+
+    PACMAN_3 = 230
+    PACMAN_3N = PACMAN_3 + NORTH
+    PACMAN_3E = PACMAN_3 + EAST
+    PACMAN_3S = PACMAN_3 + SOUTH
+    PACMAN_3W = PACMAN_3 + WEST
+
+    PACMAN_4 = 240
+    PACMAN_4N = PACMAN_4 + NORTH
+    PACMAN_4E = PACMAN_4 + EAST
+    PACMAN_4S = PACMAN_4 + SOUTH
+    PACMAN_4W = PACMAN_4 + WEST
+
+    PACMAN_5 = 250
+    PACMAN_5N = PACMAN_5 + NORTH
+    PACMAN_5E = PACMAN_5 + EAST
+    PACMAN_5S = PACMAN_5 + SOUTH
+    PACMAN_5W = PACMAN_5 + WEST
+
+    PACMAN_6 = 260
+    PACMAN_6N = PACMAN_6 + NORTH
+    PACMAN_6E = PACMAN_6 + EAST
+    PACMAN_6S = PACMAN_6 + SOUTH
+    PACMAN_6W = PACMAN_6 + WEST
+
+    GHOST_1 = 310
+    GHOST_1N = GHOST_1 + NORTH
+    GHOST_1E = GHOST_1 + EAST
+    GHOST_1S = GHOST_1 + SOUTH
+    GHOST_1W = GHOST_1 + WEST
+
+    GHOST_2 = 320
+    GHOST_2N = GHOST_2 + NORTH
+    GHOST_2E = GHOST_2 + EAST
+    GHOST_2S = GHOST_2 + SOUTH
+    GHOST_2W = GHOST_2 + WEST
+
+    GHOST_3 = 330
+    GHOST_3N = GHOST_3 + NORTH
+    GHOST_3E = GHOST_3 + EAST
+    GHOST_3S = GHOST_3 + SOUTH
+    GHOST_3W = GHOST_3 + WEST
+
+    GHOST_4 = 340
+    GHOST_4N = GHOST_4 + NORTH
+    GHOST_4E = GHOST_4 + EAST
+    GHOST_4S = GHOST_4 + SOUTH
+    GHOST_4W = GHOST_4 + WEST
+
+    GHOST_5 = 350
+    GHOST_5N = GHOST_5 + NORTH
+    GHOST_5E = GHOST_5 + EAST
+    GHOST_5S = GHOST_5 + SOUTH
+    GHOST_5W = GHOST_5 + WEST
+
+    GHOST_6 = 360
+    GHOST_6N = GHOST_6 + NORTH
+    GHOST_6E = GHOST_6 + EAST
+    GHOST_6S = GHOST_6 + SOUTH
+    GHOST_6W = GHOST_6 + WEST
 
     def __init__(self, state):
         self._height = state.getInitialLayout().getHeight()
@@ -220,28 +285,67 @@ class Frame(object):
 
         sprites = {
             Frame.PACMAN_1: Frame._cropSprite(spritesheet, 0, 0),
-            Frame.GHOST_1: Frame._cropSprite(spritesheet, 1, 0),
-            Frame.GHOST_2: Frame._cropSprite(spritesheet, 2, 0),
-            Frame.SCARED_GHOST: Frame._cropSprite(spritesheet, 3, 0),
-            Frame.FOOD: Frame._cropSprite(spritesheet, 4, 0),
-            Frame.CAPSULE: Frame._cropSprite(spritesheet, 5, 0),
+            Frame.PACMAN_1N: Frame._cropSprite(spritesheet, 0, 1),
+            Frame.PACMAN_1E: Frame._cropSprite(spritesheet, 0, 2),
+            Frame.PACMAN_1S: Frame._cropSprite(spritesheet, 0, 3),
+            Frame.PACMAN_1W: Frame._cropSprite(spritesheet, 0, 4),
 
-            Frame.WALL_NESW: Frame._cropSprite(spritesheet, 6, 0),
-            Frame.WALL_NESX: Frame._cropSprite(spritesheet, 6, 1),
-            Frame.WALL_NEXW: Frame._cropSprite(spritesheet, 6, 2),
-            Frame.WALL_NEXX: Frame._cropSprite(spritesheet, 6, 3),
-            Frame.WALL_NXSW: Frame._cropSprite(spritesheet, 7, 0),
-            Frame.WALL_NXSX: Frame._cropSprite(spritesheet, 7, 1),
-            Frame.WALL_NXXW: Frame._cropSprite(spritesheet, 7, 2),
-            Frame.WALL_NXXX: Frame._cropSprite(spritesheet, 7, 3),
-            Frame.WALL_XESW: Frame._cropSprite(spritesheet, 8, 0),
-            Frame.WALL_XESX: Frame._cropSprite(spritesheet, 8, 1),
-            Frame.WALL_XEXW: Frame._cropSprite(spritesheet, 8, 2),
-            Frame.WALL_XEXX: Frame._cropSprite(spritesheet, 8, 3),
-            Frame.WALL_XXSW: Frame._cropSprite(spritesheet, 9, 0),
-            Frame.WALL_XXSX: Frame._cropSprite(spritesheet, 9, 1),
-            Frame.WALL_XXXW: Frame._cropSprite(spritesheet, 9, 2),
-            Frame.WALL_XXXX: Frame._cropSprite(spritesheet, 9, 3),
+            Frame.GHOST_1: Frame._cropSprite(spritesheet, 1, 0),
+            Frame.GHOST_1N: Frame._cropSprite(spritesheet, 1, 1),
+            Frame.GHOST_1E: Frame._cropSprite(spritesheet, 1, 2),
+            Frame.GHOST_1S: Frame._cropSprite(spritesheet, 1, 3),
+            Frame.GHOST_1W: Frame._cropSprite(spritesheet, 1, 4),
+
+            Frame.GHOST_2: Frame._cropSprite(spritesheet, 2, 0),
+            Frame.GHOST_2N: Frame._cropSprite(spritesheet, 2, 1),
+            Frame.GHOST_2E: Frame._cropSprite(spritesheet, 2, 2),
+            Frame.GHOST_2S: Frame._cropSprite(spritesheet, 2, 3),
+            Frame.GHOST_2W: Frame._cropSprite(spritesheet, 2, 4),
+
+            Frame.GHOST_3: Frame._cropSprite(spritesheet, 3, 0),
+            Frame.GHOST_3N: Frame._cropSprite(spritesheet, 3, 1),
+            Frame.GHOST_3E: Frame._cropSprite(spritesheet, 3, 2),
+            Frame.GHOST_3S: Frame._cropSprite(spritesheet, 3, 3),
+            Frame.GHOST_3W: Frame._cropSprite(spritesheet, 3, 4),
+
+            Frame.GHOST_4: Frame._cropSprite(spritesheet, 4, 0),
+            Frame.GHOST_4N: Frame._cropSprite(spritesheet, 4, 1),
+            Frame.GHOST_4E: Frame._cropSprite(spritesheet, 4, 2),
+            Frame.GHOST_4S: Frame._cropSprite(spritesheet, 4, 3),
+            Frame.GHOST_4W: Frame._cropSprite(spritesheet, 4, 4),
+
+            Frame.GHOST_5: Frame._cropSprite(spritesheet, 5, 0),
+            Frame.GHOST_5N: Frame._cropSprite(spritesheet, 5, 1),
+            Frame.GHOST_5E: Frame._cropSprite(spritesheet, 5, 2),
+            Frame.GHOST_5S: Frame._cropSprite(spritesheet, 5, 3),
+            Frame.GHOST_5W: Frame._cropSprite(spritesheet, 5, 4),
+
+            Frame.GHOST_6: Frame._cropSprite(spritesheet, 6, 0),
+            Frame.GHOST_6N: Frame._cropSprite(spritesheet, 6, 1),
+            Frame.GHOST_6E: Frame._cropSprite(spritesheet, 6, 2),
+            Frame.GHOST_6S: Frame._cropSprite(spritesheet, 6, 3),
+            Frame.GHOST_6W: Frame._cropSprite(spritesheet, 6, 4),
+
+            Frame.FOOD: Frame._cropSprite(spritesheet, 7, 0),
+            Frame.CAPSULE: Frame._cropSprite(spritesheet, 7, 1),
+            Frame.SCARED_GHOST: Frame._cropSprite(spritesheet, 7, 2),
+
+            Frame.WALL_NESW: Frame._cropSprite(spritesheet, 8, 0),
+            Frame.WALL_NESX: Frame._cropSprite(spritesheet, 8, 1),
+            Frame.WALL_NEXW: Frame._cropSprite(spritesheet, 8, 2),
+            Frame.WALL_NEXX: Frame._cropSprite(spritesheet, 8, 3),
+            Frame.WALL_NXSW: Frame._cropSprite(spritesheet, 9, 0),
+            Frame.WALL_NXSX: Frame._cropSprite(spritesheet, 9, 1),
+            Frame.WALL_NXXW: Frame._cropSprite(spritesheet, 9, 2),
+            Frame.WALL_NXXX: Frame._cropSprite(spritesheet, 9, 3),
+            Frame.WALL_XESW: Frame._cropSprite(spritesheet, 10, 0),
+            Frame.WALL_XESX: Frame._cropSprite(spritesheet, 10, 1),
+            Frame.WALL_XEXW: Frame._cropSprite(spritesheet, 10, 2),
+            Frame.WALL_XEXX: Frame._cropSprite(spritesheet, 10, 3),
+            Frame.WALL_XXSW: Frame._cropSprite(spritesheet, 11, 0),
+            Frame.WALL_XXSX: Frame._cropSprite(spritesheet, 11, 1),
+            Frame.WALL_XXXW: Frame._cropSprite(spritesheet, 11, 2),
+            Frame.WALL_XXXX: Frame._cropSprite(spritesheet, 11, 3),
         }
 
         return sprites
@@ -306,12 +410,27 @@ class Frame(object):
             agentState = state.getAgentState(agentIndex)
             position = agentState.getPosition()
 
-            if (agentState.isPacman()):
-                tokens[position] = Frame.PACMAN_1
-            elif (agentState.isScaredGhost()):
+            if (agentState.isScaredGhost()):
                 tokens[position] = Frame.SCARED_GHOST
             else:
-                tokens[position] = Frame.GHOST_1 + (agentIndex - 1)
+                token = None
+                if (agentState.isPacman()):
+                    token = Frame.PACMAN_1
+                else:
+                    token = Frame.GHOST_1 + (agentIndex - 1) * 10
+
+                direction = agentState.getDirection()
+
+                if (direction == Directions.NORTH):
+                    token += Frame.NORTH
+                elif (direction == Directions.EAST):
+                    token += Frame.EAST
+                elif (direction == Directions.SOUTH):
+                    token += Frame.SOUTH
+                elif (direction == Directions.WEST):
+                    token += Frame.WEST
+
+                tokens[position] = token
 
         return tokens
 
