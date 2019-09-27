20005f2de300d55ad46528ccc98933c866d7e13a
Eriq Augustine <eriq.augustine@gmail.com>
2019-09-27 15:53:21 -0700

    Finally removed lookup() and fetchModuleAttribute(). With this, all the non-sanctioned reflection is removed.

--- pacai/util/reflection.py
+++ pacai/util/reflection.py
@@ -1,35 +1,4 @@
 import importlib
-import sys
-
-# TODO(eriq): I would like this method to be removed.
-# There should be better ways than reflexivly searching modules.
-def lookup(name):
-    """
-    Get a method or class from any imported module from its name.
-    """
-
-    for module_name in sys.modules:
-        try:
-            module = importlib.import_module(module_name)
-        except ImportError:
-            continue
-
-        if (name in dir(module)):
-            return getattr(module, name)
-
-    raise Exception('%s not found as a method or class' % name)
-
-def fetchModuleAttribute(name, modules):
-    """
-    Looks in the given modules for an attribute with the given name.
-    Raises an exception if the attribute is not found.
-    """
-
-    for module in modules:
-        if name in dir(module):
-            return getattr(module, name)
-
-    raise AttributeError("Could not locate attribute '%s' in modules: %s." % (name, modules))
 
 def qualifiedImport(qualified_name):
     """
