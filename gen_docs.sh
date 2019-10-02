4345549e0115f4a5e86afb2d6543b890c92d3156
Eriq Augustine <eriq.augustine@gmail.com>
2019-10-01 23:34:20 -0700

    Added pdoc3 as a requirement and updated the gen_docs.sh script to use 'pdoc3' instead of 'pdoc'.

--- gen_docs.sh
+++ gen_docs.sh
@@ -4,4 +4,4 @@ readonly THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 trap exit SIGINT
 
-pdoc --config show_inherited_members=True --html --force pacai
+pdoc3 --config show_inherited_members=True --html --force pacai
