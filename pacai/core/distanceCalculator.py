bf3d0d4d9d4c7a48ad12228348ae0fc3afb8d567
sgasparyan <supermonkeydud@gmail.com>
2019-10-15 04:34:28 -0700

    Update distanceCalculator.py
    Refactor distanceCalculatory.py by making it 1 class and deleting pointless functions

--- pacai/core/distanceCalculator.py
+++ pacai/core/distanceCalculator.py
@@ -7,35 +7,41 @@ DEFAULT_DISTANCE = 10000
 
 class Distancer(object):
     """
-    A class for computing and caching the shortest path between any two points in a given maze.
-
+    This file contains a Distancer object which computes and
+    caches the shortest path between any two points in a given maze.
     Example:
-    ```
     distancer = Distancer(gameState.getInitialLayout())
     distancer.getDistance((1, 1), (10, 10))
-    ```
     """
 
     def __init__(self, layout):
+        """
+        Initialize with Distancer (layout).
+        """
         self._distances = None
-        self.dc = DistanceCalculator(layout, self)
+        self.layout = layout
+        self.cache = {}
 
-    def getMazeDistances(self):
-        self.dc.run()
+    def getMazeDistance(self):
+        if self.layout.walls not in self.cache:
+            self.cache[self.layout.walls] = self.computeDistances(self.layout)
+
+        self._distances = self.cache[self.layout.walls]
 
     def getDistance(self, pos1, pos2):
         """
-        The only function you will need after you create the object.
+        The getDistance function is the only one you'll need after you create the object.
         """
 
         if (self._distances is None):
             return manhattan(pos1, pos2)
 
-        if isInt(pos1) and isInt(pos2):
-            return self.getDistanceOnGrid(pos1, pos2)
+        if pos1[0] == int(pos1[0]) and pos1[1] == int(pos1[1]):
+            if pos2[0] == int(pos2[0]) and pos2[1] == int(pos2[1]):
+                return self.getDistanceOnGrid(pos1, pos2)
 
-        pos1Grids = getGrids2D(pos1)
-        pos2Grids = getGrids2D(pos2)
+        pos1Grids = self.getGrids2D(pos1)
+        pos2Grids = self.getGrids2D(pos2)
         bestDistance = DEFAULT_DISTANCE
 
         for pos1Snap, snap1Distance in pos1Grids:
@@ -44,7 +50,6 @@ class Distancer(object):
                 distance = gridDistance + snap1Distance + snap2Distance
                 if bestDistance > distance:
                     bestDistance = distance
-
         return bestDistance
 
     def getDistanceOnGrid(self, pos1, pos2):
@@ -54,103 +59,70 @@ class Distancer(object):
 
         raise Exception("Position not in grid: " + str(key))
 
-    def isReadyForMazeDistance(self):
-        return (self._distances is not None)
+    def getGrids2D(self, pos):
+        grids = grid1Dx = grid1Dy = []
+        intX = int(pos[0])
+        intY = int(pos[1])
+        if pos[0] == intX:
+            grid1Dx = [(pos[0], 0)]
+        grid1Dx = [(intX, pos[0] - intX), (intX + 1, intX + 1 - pos[0])]
+        if pos[1] == intY:
+            grid1Dy = [(pos[1], 0)]
+        grid1Dy = [(intY, pos[1] - intY), (intY + 1, intY + 1 - pos[1])]
+        for x, xDistance in grid1Dx:
+            for y, yDistance in grid1Dy:
+                grids.append(((x, y), xDistance + yDistance))
+        return grids
 
-def isInt(pos):
-    x, y = pos
-    return x == int(x) and y == int(y)
+    def computeDistances(self, layout):
 
-def getGrids2D(pos):
-    grids = []
-    for x, xDistance in getGrids1D(pos[0]):
-        for y, yDistance in getGrids1D(pos[1]):
-            grids.append(((x, y), xDistance + yDistance))
-    return grids
+        distances = {}
+        allNodes = layout.walls.asList(False)
 
-def getGrids1D(x):
-    intX = int(x)
-    if x == int(x):
-        return [(x, 0)]
-    return [(intX, x - intX), (intX + 1, intX + 1 - x)]
+        for source in allNodes:
+            dist = {}
+            closed = {}
 
-##########################################
-# MACHINERY FOR COMPUTING MAZE DISTANCES #
-##########################################
+            for node in allNodes:
+                dist[node] = sys.maxsize
 
-distanceMap = {}
+            queue = priorityQueue.PriorityQueue()
+            queue.push(source, 0)
+            dist[source] = 0
 
-class DistanceCalculator:
-    def __init__(self, layout, distancer):
-        self.layout = layout
-        self.distancer = distancer
-        self.cache = {}
-
-    def run(self):
-        if self.layout.walls not in self.cache:
-            self.cache[self.layout.walls] = computeDistances(self.layout)
-
-        self.distancer._distances = self.cache[self.layout.walls]
-
-def computeDistances(layout):
-    """
-    Runs UCS to all other positions from each position.
-    """
-
-    distances = {}
-    allNodes = layout.walls.asList(False)
-
-    for source in allNodes:
-        dist = {}
-        closed = {}
-
-        for node in allNodes:
-            dist[node] = sys.maxsize
-
-        queue = priorityQueue.PriorityQueue()
-        queue.push(source, 0)
-        dist[source] = 0
-
-        while not queue.isEmpty():
-            node = queue.pop()
-            if node in closed:
-                continue
-
-            closed[node] = True
-            nodeDist = dist[node]
-            adjacent = []
-            x, y = node
-
-            if not layout.isWall((x, y + 1)):
-                adjacent.append((x, y + 1))
-
-            if not layout.isWall((x, y - 1)):
-                adjacent.append((x, y - 1))
-
-            if not layout.isWall((x + 1, y)):
-                adjacent.append((x + 1, y))
-
-            if not layout.isWall((x - 1, y)):
-                adjacent.append((x - 1, y))
-
-            for other in adjacent:
-                if other not in dist:
+            while not queue.isEmpty():
+                node = queue.pop()
+                if node in closed:
                     continue
 
-                oldDist = dist[other]
-                newDist = nodeDist + 1
-                if newDist < oldDist:
-                    dist[other] = newDist
-                    queue.push(other, newDist)
+                closed[node] = True
+                nodeDist = dist[node]
+                adjacent = []
+                x, y = node
 
-        for target in allNodes:
-            distances[(target, source)] = dist[target]
+                if not layout.isWall((x, y + 1)):
+                    adjacent.append((x, y + 1))
 
-    return distances
+                if not layout.isWall((x, y - 1)):
+                    adjacent.append((x, y - 1))
 
-def getDistanceOnGrid(distances, pos1, pos2):
-    key = (pos1, pos2)
-    if key in distances:
-        return distances[key]
+                if not layout.isWall((x + 1, y)):
+                    adjacent.append((x + 1, y))
 
-    return DEFAULT_DISTANCE
+                if not layout.isWall((x - 1, y)):
+                    adjacent.append((x - 1, y))
+
+                for other in adjacent:
+                    if other not in dist:
+                        continue
+
+                    oldDist = dist[other]
+                    newDist = nodeDist + 1
+                    if newDist < oldDist:
+                        dist[other] = newDist
+                        queue.push(other, newDist)
+
+            for target in allNodes:
+                distances[(target, source)] = dist[target]
+
+        return distances
