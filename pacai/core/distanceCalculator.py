851aaba755b1a98e6550ca015364fc8e7dc5a43e
sgasparyan <supermonkeydud@gmail.com>
2019-11-12 14:52:35 -0800

    Used round() function and getLegalNeighbors to simplify code and got rid of pointless constants

--- pacai/core/distanceCalculator.py
+++ pacai/core/distanceCalculator.py
@@ -1,6 +1,5 @@
+from pacai.core.actions import getLegalNeighbors
 from pacai.core.distance import manhattan
-
-DEFAULT_DISTANCE = 10000
 
 class Distancer(object):
     """
@@ -29,71 +28,32 @@ class Distancer(object):
         """
         The getDistance function is the only one you'll need after you create the object.
         """
-
         if (self._distances is None):
             return manhattan(pos1, pos2)
-
-        return self.getDistanceOnGrid(self.round2D(pos1), self.round2D(pos2))
-
-    def getDistanceOnGrid(self, pos1, pos2):
-        key = (pos1, pos2)
+        
+        key = ((round(pos1[0]), round(pos1[1])), (round(pos2[0]), round(pos2[1])))
         if key in self._distances:
             return self._distances[key]
 
         raise Exception("Position not in grid: " + str(key))
 
-    def round2D(self, pos):
-        intX = int(pos[0])
-        intY = int(pos[1])
-        deltaX = float(abs(pos[0] - intX))
-        deltaY = float(abs(pos[1] - intY))
-
-        if deltaX >= 0.5 and deltaY >= 0.5:
-            return (intX + 1, intY + 1)
-        elif deltaX < 0.5 and deltaY >= 0.5:
-            return (intX, intY + 1)
-        elif deltaX >= 0.5 and deltaY < 0.5:
-            return (intX + 1, intY)
-        else:
-            return (intX, intY)
-
     def computeDistances(self, layout):
-
+        """
+        layout.walls.asList(False) returns the positions on a layout that are not walls.
+        """
+        positions = layout.walls.asList(False) 
         distances = {}
-        allNodes = layout.walls.asList(False)
+        
+        for startPosition in positions:
+            queue = [startPosition]
+            distances[(startPosition, startPosition)] = 0
 
-        for source in allNodes:
-            dist = {}
-            closed = []
-
-            for node in allNodes:
-                dist[node] = DEFAULT_DISTANCE
-
-            adjacent = [source]
-            dist[source] = 0
-
-            while len(adjacent) != 0:
-                node = adjacent.pop(0)
-                if node in closed:
-                    continue
-                closed += [node]
-                x, y = node
-
-                if not layout.isWall((x, y + 1)):
-                    adjacent.append((x, y + 1))
-
-                if not layout.isWall((x, y - 1)):
-                    adjacent.append((x, y - 1))
-
-                if not layout.isWall((x + 1, y)):
-                    adjacent.append((x + 1, y))
-                if not layout.isWall((x - 1, y)):
-                    adjacent.append((x - 1, y))
-
-                for other in adjacent:
-                    if dist[other] > dist[node] + 1:
-                        dist[other] = dist[node] + 1
-            for target in allNodes:
-                distances[(target, source)] = dist[target]
+            while len(queue) != 0:
+                previousPosition = queue.pop(0)
+                queue += Actions.getLegalNeighbors(previousPosition, layout.walls)
+                oldDist = distances[(previousPosition, startPosition)]
+                for other in queue:
+                    if distances[(other, startPosition)] > oldDist + 1:
+                        distances[(other, startPosition)] = oldDist + 1
 
         return distances
