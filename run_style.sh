131ede8237ab545d294269a6845b1ab21b91f4af
lgedward <103352268+lgedward@users.noreply.github.com>
2023-05-28 16:29:31 -0700

    Delete run_style.sh

--- run_style.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-readonly THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-trap exit SIGINT
-
-python3 -m flake8 "${THIS_DIR}/pacai" --config="${THIS_DIR}/.ci/flake8.cfg"
