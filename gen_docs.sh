8168362ec8b631722ad79ae77d313ecd7a1b9a62
Eriq Augustine <eriq.augustine@gmail.com>
2024-08-06 10:35:47 +0900

    Added a CI step for generating documentation.

--- gen_docs.sh
+++ gen_docs.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 
-readonly THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# Create HTML documentation for the current code.
 
-trap exit SIGINT
+readonly THIS_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd | xargs realpath)"
 
-pdoc3 --config show_inherited_members=True --html --force pacai
+function main() {
+    if [[ $# -gt 1 ]]; then
+        echo "USAGE: $0 [out dir]"
+        exit 1
+    fi
+
+    set -e
+    trap exit SIGINT
+
+    local outputDir="${THIS_DIR}/html"
+    if [[ $# -gt 0 ]]; then
+        outputDir=$1
+    fi
+
+    cd "${THIS_DIR}"
+
+    mkdir -p "${outputDir}"
+
+    pdoc3 --config show_inherited_members=True --html --force --output-dir "${outputDir}" pacai
+
+    return 0
+}
+
+[[ "${BASH_SOURCE[0]}" == "${0}" ]] && main "$@"
